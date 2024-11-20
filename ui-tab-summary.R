af29d26558b81f3baf68c1ca5e99bbc9f3ab8739
peterleary <peter.leary@me.com>
2024-11-20 14:06:53 +0100

    More changes to the new colour uploader in exploreDE

--- ui-tab-summary.R
+++ ui-tab-summary.R
@@ -59,8 +59,19 @@ tabItem(
           width = 6,
           solidHeader = TRUE,
           status = "primary",
-          downloadButton(outputId = "dlColourTemplate", label = paste("Download Colour Template")),
-          fileInput(inputId = "importColourFile", accept = ".xlsx", width = "85%", label = "Upload Colour Template"),
+          h5("New! (November 2024)"),
+          helpText(
+            "Now you can more easily set custom colours without having to choose them each time you load the app. 
+            Download the colour template Excel file and change the colours for each factor level in the second column. 
+            You can use hex codes (e.g., #750912, #59a7cf) or ggplot colour names (e.g., dodgerblue, plum4, etc.).
+            Then, upload that file and watch the colour palette and your figures automagically update to the new colours!
+            Don't change the values in the first column, or it won't work!
+            Hopefully this is a much easier way to set colours once."
+          ),
+          splitLayout(
+            downloadButton(outputId = "dlColourTemplate", label = paste("Download Colour Template")),
+            fileInput(inputId = "importColourFile", accept = ".xlsx", width = "85%", label = NULL, placeholder = "Upload Colour Template")
+          ),
           uiOutput("colourPaletteUI"),
           uiOutput(outputId = "colourPickerUI", inline = T)
         )
