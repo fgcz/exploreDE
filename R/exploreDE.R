abf41a27b91ff04945ded16cd952305bb0378a54
Witold Wolski <wewolski@gmail.com>
2025-01-29 14:00:33 +0100

    get exploreDE function working

--- R/exploreDE.R
+++ R/exploreDE.R
@@ -5,7 +5,9 @@
 #' myDir <- "/srv/GT/analysis/peter/rs_connect_apps2/exploreDE/SummarizedExperiment.rds"
 #' exploreDE(myDir)
 #' }
-exploreDE <- function(dir = NA) {
+exploreDE <- function(fileSE = NA) {
   folder <- system.file("app", package = "exploreDE")
+  message(folder)
+  fileSE <- tools::file_path_as_absolute(fileSE)
   shiny::runApp(appDir = folder)
-  }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# explorDE
+
+## how to install
+
+```
+BiocManager::install(c("fgsea", "DESeq2", "ComplexHeatmap", "clusterProfiler", "ezRun"))
+devtools::install_github("uzh/ezRun")
+```
+
